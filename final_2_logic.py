ed93808aca43538b3d79c7a90c9b593f177b70bc
SarahNakamura <snakamura@unomaha.edu>
2023-11-29 10:27:09 -0600

    update_10:27

--- final_2_logic.py
+++ final_2_logic.py
@@ -9,5 +9,19 @@ class Logic (QMainWindow, Ui_MainWindow):
         super.__init__()
         self.setupUi(self)
 
-        self.submit.clicked.connect(lambda: self.submit())
+        self.verify.clicked.connect(lambda: self.verify_account())
+        self.submit.clicked.connect(lambda: self.account_modify())
+
+# verify account info (check name, account number, PIN)
+# display current amount if account verified
+    def verify_account(self):
+        first_name = self.input_first.lower()
+        last_name = self.input_last.lower()
+        account_num = int(self.input_account)
+        account_pin = int(self.input_pin)
+
+# deposit/withdraw amount from current account
+# display error/succession and final amount in account
+    def account_modify(self):
+        pass
 
